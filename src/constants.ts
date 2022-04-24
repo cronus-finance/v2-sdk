cde16ff2f73805e1355517b9d38a6922e7c7bd84
0xtoki <0xtoki@protonmail.com>
2022-04-24 20:58:51 +1000

    init

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x3Dc30d756A1382BF454dD6bDb48b7225CA214bB3'
+
+export const INIT_CODE_HASH = '0xea9645804ac90dfacad95722451893fc85c951fe30990c9c577bb1b636522211'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
