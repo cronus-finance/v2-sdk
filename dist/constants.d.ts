3621855b6046446f95169b703cee2157eb9da1cc
0xtoki <0xtoki@protonmail.com>
2022-04-28 06:05:19 +1000

    build

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,9 +1,9 @@
-import JSBI from 'jsbi'
-export declare const FACTORY_ADDRESS = '0x68ACB0a83AfBa44BA75F800Bf069b6ED6aAac16D'
-export declare const INIT_CODE_HASH = '0xc2c769f6cbfb568ac8562fb6e250ca27d65a3c02c8276593bd86244d6d5c6001'
-export declare const MINIMUM_LIQUIDITY: JSBI
-export declare const ZERO: JSBI
-export declare const ONE: JSBI
-export declare const FIVE: JSBI
-export declare const _997: JSBI
-export declare const _1000: JSBI
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x20570b7bFf86B2f92068622D0805160f318554Be";
+export declare const INIT_CODE_HASH = "0xc2c769f6cbfb568ac8562fb6e250ca27d65a3c02c8276593bd86244d6d5c6001";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
