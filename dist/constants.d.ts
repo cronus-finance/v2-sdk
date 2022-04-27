a97638f45680eed44a33a6e8df4188609baf702f
0xtoki <0xtoki@protonmail.com>
2022-04-28 05:50:58 +1000

    rebuild

--- dist/constants.d.ts
+++
@@ -1,9 +0,0 @@
-import JSBI from 'jsbi'
-export declare const FACTORY_ADDRESS = '0x68ACB0a83AfBa44BA75F800Bf069b6ED6aAac16D'
-export declare const INIT_CODE_HASH = '0xea9645804ac90dfacad95722451893fc85c951fe30990c9c577bb1b636522211'
-export declare const MINIMUM_LIQUIDITY: JSBI
-export declare const ZERO: JSBI
-export declare const ONE: JSBI
-export declare const FIVE: JSBI
-export declare const _997: JSBI
-export declare const _1000: JSBI
