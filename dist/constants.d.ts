edd309d1a6d6e5f82a59fb4f703bc5fe5f052995
Lint Action <lint-action@samuelmeuli.com>
2022-04-28 11:20:45 +0000

    Fix code style issues with Prettier

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,24 +1,24 @@
-import JSBI from 'jsbi';
+import JSBI from 'jsbi'
 export declare enum SupportedChainId {
-    MAINNET = 9001,
-    EVMOS_TESTNET = 9000,
-    RINKEBY = 4
+  MAINNET = 9001,
+  EVMOS_TESTNET = 9000,
+  RINKEBY = 4
 }
-export declare const FACTORY_ADDRESS = "0x20570b7bFf86B2f92068622D0805160f318554Be";
+export declare const FACTORY_ADDRESS = '0x20570b7bFf86B2f92068622D0805160f318554Be'
 export declare const FACTORY_ADDRESSES: {
-    9001: string;
-    9000: string;
-    4: string;
-};
-export declare const INIT_CODE_HASH = "0xc2c769f6cbfb568ac8562fb6e250ca27d65a3c02c8276593bd86244d6d5c6001";
+  9001: string
+  9000: string
+  4: string
+}
+export declare const INIT_CODE_HASH = '0xc2c769f6cbfb568ac8562fb6e250ca27d65a3c02c8276593bd86244d6d5c6001'
 export declare const INIT_CODE_HASHES: {
-    9001: string;
-    9000: string;
-    4: string;
-};
-export declare const MINIMUM_LIQUIDITY: JSBI;
-export declare const ZERO: JSBI;
-export declare const ONE: JSBI;
-export declare const FIVE: JSBI;
-export declare const _997: JSBI;
-export declare const _1000: JSBI;
+  9001: string
+  9000: string
+  4: string
+}
+export declare const MINIMUM_LIQUIDITY: JSBI
+export declare const ZERO: JSBI
+export declare const ONE: JSBI
+export declare const FIVE: JSBI
+export declare const _997: JSBI
+export declare const _1000: JSBI
